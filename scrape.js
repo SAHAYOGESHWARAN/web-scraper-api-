ae290e87f73997dbc824f0f5aab99b6fa1a2b322
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-18 08:41:49 +0530

    web scraper

--- scrape.js
+++ scrape.js
@@ -4,7 +4,7 @@ const scrape = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://www.example.com');
+    await page.goto('https://books.toscrape.com');
 
     const title = await page.title();
     console.log(`Page title: ${title}`); 
